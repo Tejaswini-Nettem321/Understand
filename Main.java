b8c2ff26c3fb8fc7a60700089b410a052ca11322
Tejaswini <tejaswininettem2022@gmail.com>
2024-01-08 13:40:54 +0530

    new Change

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hii");
         System.out.println("Next Day");
+        System.out.println("Have a nice day!");
+        System.out.println("Bye");
     }
     
 }
